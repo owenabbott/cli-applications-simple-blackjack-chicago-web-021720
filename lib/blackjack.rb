f0f1bf801276480dc5b8a6346029870767147f67
Owen Abbott <ocabbott@gmail.com>
2020-02-19 15:49:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,8 +29,8 @@ def get_user_input
   # code #get_user_input here
 end
 
-def end_game
-  puts "Sorry, you hit 27. Thanks for playing!"
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
   # code #end_game here
 end
 
